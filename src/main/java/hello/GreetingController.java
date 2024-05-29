1ffbcc576e7d393ed5ef9a95dd4e9cef4e2eeb30
Rodrigo Alonzo <rodrigo.pvigil@alumnos.upm.es>
2024-05-29 11:43:32 +0200

    make the application executable issue #1

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,14 +1,9 @@
 package hello;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-@Controller
-public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
-                           String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
 }
